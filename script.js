85660c54e73a07dcb51ceb7cbc55fae104e33922
Alina Rosa <rosalina@palitechnika.com>
2026-05-28 22:37:06 +0200

    holy mother of html hacks

--- script.js
+++ script.js
@@ -4,11 +4,32 @@ const t2 = document.getElementById("t2")
 const t3 = document.getElementById("t3")
 const t4 = document.getElementById("t4")
 const t5 = document.getElementById("t5")
+const allTracks = [dummy, t1, t2, t3, t4, t5]
+
+let animationEndListener = null
 
 function playAnim() {
-    t1.addEventListener("animationend", () => {
-        setNewTracks()
+    if (animationEndListener) {
+        t1.removeEventListener("animationend", animationEndListener)
+    }
+
+    // reset anims
+    allTracks.forEach(track => {
+        track.style.animation = "none"
     })
+
+    // force refolw
+    void dummy.offsetWidth
+
+    // clean in-line shit
+    allTracks.forEach(track => {
+        track.style.animation = ""
+    })
+
+    animationEndListener = () => {
+        setNewTracks()
+    }
+    t1.addEventListener("animationend", animationEndListener, { once: true })
 
     dummy.style.animationPlayState = "running"
     t1.style.animationPlayState = "running"
@@ -16,7 +37,6 @@ function playAnim() {
     t3.style.animationPlayState = "running"
     t4.style.animationPlayState = "running"
     t5.style.animationPlayState = "running"
-    
 }
 
 function setNewTracks() {
