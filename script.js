ac6cdc8b1ced8fdd80354f6deb1233b44e9fd715
Alina Rosa <rosalina@palitechnika.com>
2026-05-29 00:05:59 +0200

    add images, fix track counting

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let animationEndListener = null
 let playing = false
 
 function playAnim() {
-    if (finished) {
+    if (track > 29) {   // 29 is last coz we count from 0, and also track = 30 coz we increment after the anim
         alert("Finished!")
         return
     }
@@ -61,7 +61,7 @@ const TRACKS = [
     "Desert Hills",
     "Shy Guy Bazaar",
     "Wario Stadium",
-    "Aiship Fortress",
+    "Airship Fortress",
     "DK Pass",
     "Starview Peak",
     "Sky-High Sundae",
@@ -88,7 +88,6 @@ const TRACKS = [
 
 let shuffledTracks = []
 let track = 0
-let finished = false
 
 function setSeed() {
     if (track > 0) {
@@ -97,26 +96,26 @@ function setSeed() {
     }
     const num = document.getElementById("seed-input").value
     shuffledTracks = shuffle(TRACKS, num)
-    console.log(shuffledTracks)
+    // console.log(shuffledTracks)
 }
 
 function shuffleTracks() {
     // random seed
     let seed = Math.floor(Math.random() * 1000000)
+    console.log("Seed", seed)
     shuffledTracks = shuffle(TRACKS, seed)
-    console.log(shuffledTracks)
+    // console.log(shuffledTracks)
 }
 
 function setNewTrack() {
     if (track < 30) {
         const newTrack = shuffledTracks[track]
+        // console.log(track, newTrack)
         dummy.innerHTML = newTrack
+        // replace coz you can't have ? in filename? didn't knwo that ngl
+        document.querySelector("img").src = `./track_images/${newTrack.replace(/\?/g, '_')}.png`
         track++
-    } else {
-        // finish logic
-        finished = true
     }
-
 }
 
 function setTracks() {
@@ -175,145 +174,145 @@ function random(seed) {
  */
 
 var Konami = function (callback) {
-  var konami = {
-    addEvent: function (obj, type, fn, ref_obj) {
-      if (obj.addEventListener) obj.addEventListener(type, fn, false);
-      else if (obj.attachEvent) {
-        // IE
-        obj["e" + type + fn] = fn;
-        obj[type + fn] = function () {
-          obj["e" + type + fn](window.event, ref_obj);
-        };
-        obj.attachEvent("on" + type, obj[type + fn]);
-      }
-    },
-    removeEvent: function (obj, eventName, eventCallback) {
-      if (obj.removeEventListener) {
-        obj.removeEventListener(eventName, eventCallback);
-      } else if (obj.attachEvent) {
-        obj.detachEvent(eventName);
-      }
-    },
-    input: "",
-    pattern: "38384040373937396665",
-    keydownHandler: function (e, ref_obj) {
-      if (ref_obj) {
-        konami = ref_obj;
-      } // IE
-      konami.input += e ? e.keyCode : event.keyCode;
-      if (konami.input.length > konami.pattern.length) {
-        konami.input = konami.input.substr(
-          konami.input.length - konami.pattern.length,
-        );
-      }
-      if (konami.input === konami.pattern) {
-        konami.code(konami._currentLink);
-        konami.input = "";
-        e.preventDefault();
-        return false;
-      }
-    },
-    load: function (link) {
-      this._currentLink = link;
-      this.addEvent(document, "keydown", this.keydownHandler, this);
-      this.iphone.load(link);
-    },
-    unload: function () {
-      this.removeEvent(document, "keydown", this.keydownHandler);
-      this.iphone.unload();
-    },
-    code: function (link) {
-      window.location = link;
-    },
-    iphone: {
-      start_x: 0,
-      start_y: 0,
-      stop_x: 0,
-      stop_y: 0,
-      tap: false,
-      capture: false,
-      orig_keys: "",
-      keys: [
-        "UP",
-        "UP",
-        "DOWN",
-        "DOWN",
-        "LEFT",
-        "RIGHT",
-        "LEFT",
-        "RIGHT",
-        "TAP",
-        "TAP",
-      ],
-      input: [],
-      code: function (link) {
-        konami.code(link);
-      },
-      touchmoveHandler: function (e) {
-        if (e.touches.length === 1 && konami.iphone.capture === true) {
-          var touch = e.touches[0];
-          konami.iphone.stop_x = touch.pageX;
-          konami.iphone.stop_y = touch.pageY;
-          konami.iphone.tap = false;
-          konami.iphone.capture = false;
-        }
-      },
-      touchendHandler: function () {
-        konami.iphone.input.push(konami.iphone.check_direction());
-
-        if (konami.iphone.input.length > konami.iphone.keys.length)
-          konami.iphone.input.shift();
-
-        if (konami.iphone.input.length === konami.iphone.keys.length) {
-          var match = true;
-          for (var i = 0; i < konami.iphone.keys.length; i++) {
-            if (konami.iphone.input[i] !== konami.iphone.keys[i]) {
-              match = false;
+    var konami = {
+        addEvent: function (obj, type, fn, ref_obj) {
+            if (obj.addEventListener) obj.addEventListener(type, fn, false);
+            else if (obj.attachEvent) {
+                // IE
+                obj["e" + type + fn] = fn;
+                obj[type + fn] = function () {
+                    obj["e" + type + fn](window.event, ref_obj);
+                };
+                obj.attachEvent("on" + type, obj[type + fn]);
             }
-          }
-          if (match) {
-            konami.iphone.code(konami._currentLink);
-          }
-        }
-      },
-      touchstartHandler: function (e) {
-        konami.iphone.start_x = e.changedTouches[0].pageX;
-        konami.iphone.start_y = e.changedTouches[0].pageY;
-        konami.iphone.tap = true;
-        konami.iphone.capture = true;
-      },
-      load: function (link) {
-        this.orig_keys = this.keys;
-        konami.addEvent(document, "touchmove", this.touchmoveHandler);
-        konami.addEvent(document, "touchend", this.touchendHandler, false);
-        konami.addEvent(document, "touchstart", this.touchstartHandler);
-      },
-      unload: function () {
-        konami.removeEvent(document, "touchmove", this.touchmoveHandler);
-        konami.removeEvent(document, "touchend", this.touchendHandler);
-        konami.removeEvent(document, "touchstart", this.touchstartHandler);
-      },
-      check_direction: function () {
-        var x_magnitude = Math.abs(this.start_x - this.stop_x);
-        var y_magnitude = Math.abs(this.start_y - this.stop_y);
-        var x = this.start_x - this.stop_x < 0 ? "RIGHT" : "LEFT";
-        var y = this.start_y - this.stop_y < 0 ? "DOWN" : "UP";
+        },
+        removeEvent: function (obj, eventName, eventCallback) {
+            if (obj.removeEventListener) {
+                obj.removeEventListener(eventName, eventCallback);
+            } else if (obj.attachEvent) {
+                obj.detachEvent(eventName);
+            }
+        },
+        input: "",
+        pattern: "38384040373937396665",
+        keydownHandler: function (e, ref_obj) {
+            if (ref_obj) {
+                konami = ref_obj;
+            } // IE
+            konami.input += e ? e.keyCode : event.keyCode;
+            if (konami.input.length > konami.pattern.length) {
+                konami.input = konami.input.substr(
+                    konami.input.length - konami.pattern.length,
+                );
+            }
+            if (konami.input === konami.pattern) {
+                konami.code(konami._currentLink);
+                konami.input = "";
+                e.preventDefault();
+                return false;
+            }
+        },
+        load: function (link) {
+            this._currentLink = link;
+            this.addEvent(document, "keydown", this.keydownHandler, this);
+            this.iphone.load(link);
+        },
+        unload: function () {
+            this.removeEvent(document, "keydown", this.keydownHandler);
+            this.iphone.unload();
+        },
+        code: function (link) {
+            window.location = link;
+        },
+        iphone: {
+            start_x: 0,
+            start_y: 0,
+            stop_x: 0,
+            stop_y: 0,
+            tap: false,
+            capture: false,
+            orig_keys: "",
+            keys: [
+                "UP",
+                "UP",
+                "DOWN",
+                "DOWN",
+                "LEFT",
+                "RIGHT",
+                "LEFT",
+                "RIGHT",
+                "TAP",
+                "TAP",
+            ],
+            input: [],
+            code: function (link) {
+                konami.code(link);
+            },
+            touchmoveHandler: function (e) {
+                if (e.touches.length === 1 && konami.iphone.capture === true) {
+                    var touch = e.touches[0];
+                    konami.iphone.stop_x = touch.pageX;
+                    konami.iphone.stop_y = touch.pageY;
+                    konami.iphone.tap = false;
+                    konami.iphone.capture = false;
+                }
+            },
+            touchendHandler: function () {
+                konami.iphone.input.push(konami.iphone.check_direction());
 
-        var result =
-          this.tap === true ? "TAP" : x_magnitude > y_magnitude ? x : y;
+                if (konami.iphone.input.length > konami.iphone.keys.length)
+                    konami.iphone.input.shift();
 
-        return result;
-      },
-    },
-  };
+                if (konami.iphone.input.length === konami.iphone.keys.length) {
+                    var match = true;
+                    for (var i = 0; i < konami.iphone.keys.length; i++) {
+                        if (konami.iphone.input[i] !== konami.iphone.keys[i]) {
+                            match = false;
+                        }
+                    }
+                    if (match) {
+                        konami.iphone.code(konami._currentLink);
+                    }
+                }
+            },
+            touchstartHandler: function (e) {
+                konami.iphone.start_x = e.changedTouches[0].pageX;
+                konami.iphone.start_y = e.changedTouches[0].pageY;
+                konami.iphone.tap = true;
+                konami.iphone.capture = true;
+            },
+            load: function (link) {
+                this.orig_keys = this.keys;
+                konami.addEvent(document, "touchmove", this.touchmoveHandler);
+                konami.addEvent(document, "touchend", this.touchendHandler, false);
+                konami.addEvent(document, "touchstart", this.touchstartHandler);
+            },
+            unload: function () {
+                konami.removeEvent(document, "touchmove", this.touchmoveHandler);
+                konami.removeEvent(document, "touchend", this.touchendHandler);
+                konami.removeEvent(document, "touchstart", this.touchstartHandler);
+            },
+            check_direction: function () {
+                var x_magnitude = Math.abs(this.start_x - this.stop_x);
+                var y_magnitude = Math.abs(this.start_y - this.stop_y);
+                var x = this.start_x - this.stop_x < 0 ? "RIGHT" : "LEFT";
+                var y = this.start_y - this.stop_y < 0 ? "DOWN" : "UP";
 
-  typeof callback === "string" && konami.load(callback);
-  if (typeof callback === "function") {
-    konami.code = callback;
-    konami.load();
-  }
+                var result =
+                    this.tap === true ? "TAP" : x_magnitude > y_magnitude ? x : y;
 
-  return konami;
+                return result;
+            },
+        },
+    };
+
+    typeof callback === "string" && konami.load(callback);
+    if (typeof callback === "function") {
+        konami.code = callback;
+        konami.load();
+    }
+
+    return konami;
 };
 
 const easterEgg = Konami(() => toggleAdmin())
